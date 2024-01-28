4dd81504d2888df2f3a7c7d908d83f90480b5edb
opetry16 <everlands.op@gmail.com>
2024-01-28 13:58:55 +0100

    V1.4

--- Exercise 1/main.py
+++ Exercise 1/main.py
@@ -2,36 +2,34 @@ import argparse
 from pathlib import Path
 import shutil
 
-
 def parse_argv():
-    parser = argparse.ArgumentParser(description="Рекурсивно копіює файли та сортує їх за розширенням.")
-    parser.add_argument("-s", "--source", type=Path, required=True, help="Шлях до вихідної директорії")
-    parser.add_argument("-d", "--destination", type=Path, default=Path("dist"), help="Шлях до директорії призначення (за замовчуванням: dist)")
+    parser = argparse.ArgumentParser(description="Копіює файли в папку")
+    parser.add_argument(
+        "-s", "--source", type=Path, required=True, help="Шлях до вихідної директорії"
+    )
+    parser.add_argument(
+        "-o", "--output", type=Path, default=Path("dist"), help="Шлях до директорії призначення"
+    )
     return parser.parse_args()
 
-
-def recursive_copy(source: Path, destination: Path):
+def recursive_copy(source: Path, output: Path):
     try:
+        output.mkdir(exist_ok=True, parents=True)
         for el in source.iterdir():
             if el.is_dir():
-                recursive_copy(el, destination)
+                recursive_copy(el, output)
             else:
-                file_extension = el.suffix[1:]  # отримуємо розширення файлу
-                folder = destination / file_extension
+                folder_name = el.suffix.lstrip('.')  # використовуємо розширення як ім'я піддиректорії
+                folder = output / folder_name
                 folder.mkdir(exist_ok=True)
                 shutil.copy(el, folder)
     except Exception as e:
-        print(f"Помилка обробки директорії {source}: {e}")
+        print(f"Помилка: {e}")
 
 def main():
     args = parse_argv()
-    
-    try:
-        # Рекурсивне копіювання файлів
-        recursive_copy(args.source, args.destination)
-        print("Копіювання файлів та сортування за розширенням завершено успішно.")
-    except Exception as e:
-        print(f"Помилка: {e}")
+    recursive_copy(args.source, args.output)
+    print("Копіювання та сортування завершено.")
 
 if __name__ == "__main__":
     main()
